a32b74ab00fd060b164908ee7fd72126b4c2619b
07602520 <raymon3838@gmail.com>
2017-03-13 20:31:05 +0800

    Add files via upload

---
+++ HelloWorld.ino
@@ -0,0 +1,30 @@
+const byte LED = 13;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello,World!");
+  Serial.print("\tLED pin is: ");
+  Serial.print(LED);
+  Serial.println("\n");
+  pinMode(LED,OUTPUT);
+}
+
+void loop() 
+{
+  char val;
+  if(Serial.available())
+  {
+    val = Serial.read();
+    switch(val)
+    {
+      
+      case'0':
+      digitalWrite(13,LOW);
+      Serial.println("LED OFF");
+      break;
+      case'1':
+      digitalWrite(13,HIGH);
+      Serial.println("LED ON");
+      break;
+    }
+  }
+}
